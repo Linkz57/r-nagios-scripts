c84f48f357133b905b9acf306a975d4c26b505da
Tyler Francis <Linkz57@gmail.com>
2018-09-24 09:47:29 -0500

    Send a single email containing several days' info
    The grep in line 20 removes some common complaints tar has about failing to backup things I don't really want backed up.

---
+++ backup_nagios/mailbackups.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## mailbackups.sh
+## version 1.1
+
+if cd "~/scripts" ; then
+        echo fine > /dev/null
+else
+        echo "could not cd to ~/scripts"
+        exit 1
+fi
+pwd -P
+
+
+echo "I am three scripts running on $(hostname) in $(pwd -P)" >> backup.mail
+echo "We are named backup_nagios.sh, cleanup_nagios_backups.sh, and mailbackups.sh" >> backup.mail
+echo "To fiddle with us run:" >> backup.mail
+echo "ssh -t someSSHaccount@$(hostname) \"sudo crontab -eu $(whoami)\"" >> backup.mail
+
+cat backup.mail | grep -v "from member names\|/usr/local/nagios/var" | mail -s "Weekly results of daily Nagios backups" tyler dot francis at jelec dot com
+
+\rm -f backup.mail
